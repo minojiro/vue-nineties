585f8131a5835832014b6388732188acffca247c
minojiro <minojiro@users.noreply.github.com>
2022-03-07 11:58:17 +0900

    chore: remove redundant css import

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import "./styles/style.css";
 export { default as Blink } from "./components/Blink/index.vue";
 export { default as Button } from "./components/Button/index.vue";
 export { default as Marquee } from "./components/Marquee/index.vue";
